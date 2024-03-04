4dcc5405560d17e65a54b88f52bd21a2a4247873
k.b.Yudhees Raam Kumar <yudees479@gmail.com>
2024-03-04 12:04:33 +0530

    controller

---
+++ src/vuecontrollerserviceprovider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Yudhees\LaravelVueController;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Yudhees\LaravelVueController\vuecontroller;
+class vuecontrollerserviceprovider extends ServiceProvider{
+    public function boot()
+    {
+        $this->registerRoutes();
+    }
+
+    protected function registerRoutes()
+    {
+        Route::post('/vuecontroller/{controller}/{method}',vuecontroller::class)->name('vuecontroller');
+    }   
+}
